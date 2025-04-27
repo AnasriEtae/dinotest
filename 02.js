d01e0e3c5f1995928f13f0c4f479158429e953f9
Anasri Etae <AnasriEtae>
2025-04-27 13:24:23 +0700

    first commit

---
+++ 02.js
@@ -0,0 +1,9 @@
+function showSearchMenu() {
+    let searchMenu = document.getElementById('searchMenu');
+
+    if (searchMenu.style.display === 'none') {
+        searchMenu.style.display = 'block';
+    } else {
+        searchMenu.style.display = 'none';
+    }
+}
